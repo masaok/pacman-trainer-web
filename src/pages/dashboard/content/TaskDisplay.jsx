edbf3c47398e7ab14488ec514bf025fd8b792999
Masao Kitamura <masaok@gmail.com>
2020-11-24 20:23:26 -0800

    Maze generation is more even

--- src/pages/dashboard/content/TaskDisplay.jsx
+++ src/pages/dashboard/content/TaskDisplay.jsx
@@ -1,18 +1,24 @@
-import React, {
-  useEffect,
-  // useState
-} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
 
 import clsx from 'clsx'
 
 import { makeStyles } from '@material-ui/core/styles'
 
+import Button from '@material-ui/core/Button'
+
 import { SITE_TITLE_POSTFIX } from '../../../constants'
 
 const useStyles = makeStyles(
   theme => ({
     root: {
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+
+    mazeContainer: {
       display: 'flex',
       flex: 1,
       flexDirection: 'column',
@@ -53,6 +59,14 @@ const useStyles = makeStyles(
     mazeError: {
       backgroundColor: 'red',
     },
+
+    // Controls
+    controlArea: {
+      display: 'flex',
+      flex: 1,
+      marginTop: theme.spacing(3),
+      justifyContent: 'center',
+    },
   }),
   { name: 'TaskDisplay' }
 )
@@ -68,12 +82,106 @@ const mockMaze = [
   ['X', 'X', 'X', 'X', 'X', 'X', 'X'],
 ]
 
+// Common tokens (Pacman and Ghosts are special)
+const tokens = [
+  'X', // wall
+  ' ', // space
+]
+
+const maxGhosts = 3
+const maxPacmen = 1
+
 const TaskDisplay = props => {
   const classes = useStyles(props)
+
+  const [maze, setMaze] = useState(mockMaze)
 
   useEffect(() => {
     console.log('TASK > FIRST LOAD EFFECT')
   }, [])
+
+  const isTokenValid = (token, numGhosts, numPacmen) => {
+    switch (token) {
+      case 'G':
+        return numGhosts < maxGhosts
+      case 'P':
+        return numPacmen < maxPacmen
+      default:
+        return true
+    }
+  }
+
+  const generateNonRepeatingRandomNumbers = (howMany, maxNumber, invalid = []) => {
+    const indexes = []
+    for (let i = 0; i < howMany; i++) {
+      while (true) {
+        let index = Math.floor(Math.random() * maxNumber)
+        if (!indexes.includes(index) && !invalid.includes(index)) {
+          indexes.push(index)
+          break
+        }
+      }
+    }
+    return indexes
+  }
+
+  const generateNewMaze = () => {
+    const width = 7
+    const height = 8
+
+    const maxTokens = width * height
+
+    // Generate evenly-distributed random ghost locations
+    const ghostIndexes = generateNonRepeatingRandomNumbers(maxGhosts, maxTokens)
+    console.log('GHOST INDEXES:')
+    console.log(ghostIndexes)
+
+    // Pacman cannot be placed on an existing ghost
+    const pacmanIndexes = generateNonRepeatingRandomNumbers(maxPacmen, maxTokens, ghostIndexes)
+    console.log('PACMAN INDEXES:')
+    console.log(pacmanIndexes)
+
+    let newMaze = []
+    let numGhosts = 0
+    let numPacmen = 0
+    let currentIndex = 0
+    for (let row = 0; row < height; row++) {
+      let rowItems = []
+      for (let col = 0; col < width; col++) {
+        let token = null
+        if (pacmanIndexes.includes(currentIndex)) {
+          token = 'P'
+        } else if (ghostIndexes.includes(currentIndex)) {
+          token = 'G'
+        } else {
+          while (true) {
+            token = tokens[Math.floor(Math.random() * tokens.length)]
+            console.log('RANDOM TOKEN: ' + token)
+            if (isTokenValid(token, numGhosts, numPacmen)) break
+          }
+
+          switch (token) {
+            case 'X':
+              break
+            case ' ':
+              break
+            case 'P':
+              numPacmen++
+              break
+            case 'G':
+              numGhosts++
+              break
+            default:
+              rowItems.push('E') // error
+          }
+        }
+        rowItems.push(token)
+        currentIndex++
+      }
+      newMaze.push(rowItems)
+    }
+    setMaze(newMaze)
+  }
 
   return (
     <div className={classes.root}>
@@ -81,10 +189,10 @@ const TaskDisplay = props => {
         <title>Task Display {SITE_TITLE_POSTFIX}</title>
       </Helmet>
       <div className={classes.mazeContainer}>
-        {mockMaze.map((row, rowIndex) => {
+        {maze.map((row, rowIndex) => {
           return (
             <div className={classes.mazeRow}>
-              {mockMaze[rowIndex].map((item, colIndex) => {
+              {maze[rowIndex].map((item, colIndex) => {
                 switch (item) {
                   case ' ':
                     return <div className={clsx(classes.mazeBlock, classes.mazeSpace)}>{item}</div>
@@ -101,6 +209,11 @@ const TaskDisplay = props => {
             </div>
           )
         })}
+      </div>
+      <div className={classes.controlArea}>
+        <Button variant="contained" color="primary" onClick={generateNewMaze}>
+          GENERATE NEW MAZE
+        </Button>
       </div>
     </div>
   )
